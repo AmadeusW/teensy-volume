2515d12d11381c7c2349a936a6a3a3ebeb5d3b79
Amadeusz Wieczorek <git@amadeusw.com>
2017-03-25 12:17:49 -0700

    more complex detection

--- src/volume/volume.ino
+++ src/volume/volume.ino
@@ -1,13 +1,19 @@
 int reading;
 int smoothReading;
 int initializationStep; // we take 10 measurements to initialize
+int centerReading;
+int bottomReading;
+int topReading;
 int defaultReading; // idle reading
 int defaultReadingSum; // used to calculate defaultReading
 bool sentSingleCommand; // used for debouncing mute and play buttons
 
 // State machine
 int state;
-const int INITIALIZING = 0; // go to 1
+const int INITIALIZING = 0; // go to 6
+const int DETECTCENTER = 6; // go to 7
+const int DETECTBOTTOM = 7; // go to 8
+const int DETECTTOP = 8; // go to 1
 const int IDLE = 1; // go to 2,3,4,5
 const int VOLUMEDOWN = 2; // go to 1
 const int VOLUMEUP = 3; // go to 1
@@ -40,19 +46,28 @@ void loop() {
   if (state == INITIALIZING)
   {
     initialize();
-    digitalWrite(ledPin, initializationStep % 2 == 0 ? HIGH : LOW);
+  }
+  else if (state == DETECTCENTER)
+  {
+    detectCenter();
+  }
+  else if (state == DETECTBOTTOM)
+  {
+    detectBottom();
+  }
+  else if (state == DETECTTOP)
+  {
+    detectTop();
   }
   else if (state == IDLE)
   {
-    digitalWrite(ledPin, LOW);
     measure();
   }
   else
   {
-    digitalWrite(ledPin, HIGH);
     sendKeystroke();
   }
-  Serial.println(reading);
+  //Serial.println(reading);
 }
 
 void initialize()
@@ -64,29 +79,104 @@ void initialize()
     defaultReading = defaultReadingSum / 10;
     Serial.print("Default reading: ");
     Serial.println(defaultReading);
+    state = DETECTCENTER;
+  }
+  delay(100);
+}
+
+void detectCenter()
+{
+  digitalWrite(ledPin, HIGH);
+  Serial.print("Hold hand in the center...");
+  delay(2000);
+  reading = analogRead(dataPin);
+  smoothReading = reading;
+
+  for (int i = 4; i > 0; i--)
+  {
+    digitalWrite(ledPin, LOW);
+    delay(i * 100);
+    reading = analogRead(dataPin);
+    smoothReading = (smoothReading + reading) / 2;
+  
+    digitalWrite(ledPin, HIGH);
+    delay(i * 100);
+    reading = analogRead(dataPin);
+    smoothReading = (smoothReading + reading) / 2;    
+  }
+
+  digitalWrite(ledPin, LOW);
+  centerReading = smoothReading;
+  topReading = centerReading * 1.2;
+  bottomReading = centerReading * 0.8;
+  Serial.print("Center reading: ");
+  Serial.println(centerReading);
+  state = DETECTBOTTOM;
+  Serial.println("Hold hand at the bottom...");
+  delay(500);
+}
+
+void detectBottom()
+{
+  if (smoothReading < bottomReading)
+  {
+    digitalWrite(ledPin, HIGH);
+    bottomReading = smoothReading;
+    Serial.print("Lowest reading:");
+    Serial.println(bottomReading);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  delay(100);
+
+  if (smoothReading > centerReading * 1.2)
+  {
+    state = DETECTTOP;
+    Serial.println("Hold hand at the top...");
+  }
+}
+
+void detectTop()
+{
+  if (smoothReading > topReading)
+  {
+    digitalWrite(ledPin, HIGH);
+    topReading = smoothReading;
+    Serial.print("Highest reading:");
+    Serial.println(topReading);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  delay(100);
+
+  if (smoothReading < centerReading * 0.8)
+  {
+    Serial.println("All done!");
     state = IDLE;
   }
-  delay(50);
 }
 
 void measure()
 {
-  Serial.println(smoothReading);
-  if (smoothReading > defaultReading + 150)
+  digitalWrite(ledPin, LOW);
+  if (smoothReading > (centerReading + topReading * 4) / 5)
   {
     state = PLAYPAUSE;
     sentSingleCommand = false;
   }
-  else if (smoothReading > defaultReading + 50)
+  else if (smoothReading > (centerReading + topReading) / 2)
   {
     state = VOLUMEUP;
   }
-  else if (smoothReading < defaultReading - 150)
+  else if (smoothReading < (centerReading + bottomReading * 4) / 5)
   {
     state = VOLUMEMUTE;
-    sentSingleCommand = false;
   }
-  else if (smoothReading < defaultReading - 50)
+  else if (smoothReading < (centerReading + bottomReading) / 2)
   {
     state = VOLUMEDOWN;
   }
@@ -94,6 +184,7 @@ void measure()
 
 void sendKeystroke()
 {
+  digitalWrite(ledPin, HIGH);
   switch (state)
   {
     case PLAYPAUSE:
